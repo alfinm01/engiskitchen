1a0e145372bb7735f81d666c3d565c6dcc5aa00e
ARadhitama <ariqradhitamaa@gmail.com>
2018-11-27 22:26:24 +0700

    UI kelar

--- UI/UI.h
+++ UI/UI.h
@@ -1,21 +1,16 @@
 /* ADT print UI */
+#include "matriks.h"
+#include "stdio.h"
 
 #ifndef _UI_H_
 #define _UI_H_
 
 #include "../kamus.h"
 //#include "../ADT/mesin_kata.h"
-
-void UIGameHeader();
-
-void UIMainRoom();
-/*	I.S Sembarang
-	F.S Menampilkan UI Main Room */
-	
-
-void UIKitchen();
-/*	I.S Sembarang	
-	F.S Menampilkan UI Kitchen */
+void AssignMatriks(MATRIKS *M, char JenisRuang);
+void printBatas();
+void Map(MATRIKS *M, char JenisRuang);
+void PrintUI(MATRIKS *M, char JenisRuang);
 
 void ReadState(States *State);
 
